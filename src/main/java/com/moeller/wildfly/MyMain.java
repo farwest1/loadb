651f4017bbe089884372e025e0aa89e83472dbc1
Bernd <moeller-bernd@gmx.de>
2017-01-21 18:32:47 +0100

    Try to use heroku postgres db

---
+++ src/main/java/com/moeller/wildfly/MyMain.java
@@ -0,0 +1,16 @@
+package com.moeller.wildfly;
+
+import org.wildfly.swarm.datasources.DatasourcesFraction;
+
+/**
+ * Created by Bernd on 21.01.2017.
+ *
+ * Package com.moeller.wildfly
+ */
+public class MyMain {
+
+  public static void main(String...args){
+
+  }
+
+}
